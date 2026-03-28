fcced0f5c04728980d02d9591e6f85c5d1642c7c
Sajid Hasan Dipto <sajidhasandipto.work@gmail.com>
2026-03-28 22:12:48 +0600

    feat(Navbar): implementing Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,25 +1,47 @@
 import React from 'react'
-import {navLinks} from '../constants/index'
+import { navLinks } from '../constants/index'
+import { useGSAP } from '@gsap/react'
+import gsap from 'gsap'
 
 const Navbar = () => {
-  return (
-    <nav>
-        <div>
-            <a href='#home' className='flex items-center gap-2'>
-                <img src='/images/logo.png' alt='logo' />
-                <p>Valvet Pour</p>
-            </a>
 
-            <ul>
-                {navLinks.map((link)=>(
-                    <li key={link.id}>
-                        <a href={`#${link.id}`}>{link.title}</a>
-                    </li>
-                ))}
-            </ul>
-        </div>
-    </nav>
-  )
+    useGSAP(() => {
+        const navTween = gsap.timeline({
+            scrollTrigger: {
+                trigger: 'nav',
+                start: 'bottom top'
+            }
+        });
+
+        navTween.fromTo('nav', {
+            backgroundColor: 'transparent'
+        },
+        {
+            backgroundColor: '#00000050',
+            backdropFilter: 'blur(10px)',
+            duration: 1,
+            ease:'power1.inOut'
+       })
+    })
+
+    return (
+        <nav>
+            <div>
+                <a href='#home' className='flex items-center gap-2'>
+                    <img src='/images/logo.png' alt='logo' />
+                    <p>Valvet Pour</p>
+                </a>
+
+                <ul>
+                    {navLinks.map((link) => (
+                        <li key={link.id}>
+                            <a href={`#${link.id}`}>{link.title}</a>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        </nav>
+    )
 }
 
 export default Navbar
